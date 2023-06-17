68fe245102112cd491c2d622c05fff6537ffa248
Kiwagu <kiwagu@gmail.com>
2023-06-17 17:56:39 +0200

    feat(kiosk): add auto redirection from host ui to kiosk one

--- apps/gateway/apps/ui-host/src/app/app.component.ts
+++ apps/gateway/apps/ui-host/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'deepq-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ui-host';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Queue the navigation after initialNavigation blocking is completed
+    setTimeout(() => {
+      this.router.navigateByUrl('kiosk');
+    });
+  }
 }
