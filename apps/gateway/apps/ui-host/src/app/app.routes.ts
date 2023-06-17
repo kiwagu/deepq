16582f9c7978bc56e68efef0dd84ff223c69c959
Kiwagu <kiwagu@gmail.com>
2023-06-17 17:49:19 +0200

    fix(kiosk): replace dynamic mf to static one

--- apps/gateway/apps/ui-host/src/app/app.routes.ts
+++ apps/gateway/apps/ui-host/src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Route } from '@angular/router';
-import { loadRemoteModule } from '@nx/angular/mf';
 
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 export const appRoutes: Route[] = [
   {
     path: 'kiosk',
-    loadChildren: () =>
-      loadRemoteModule('kiosk', './Module').then((m) => m.RemoteEntryModule),
+    loadChildren: () => import('kiosk/Module').then((m) => m.RemoteEntryModule),
   },
   {
     path: '',
